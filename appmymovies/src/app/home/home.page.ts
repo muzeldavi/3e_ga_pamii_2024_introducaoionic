188c2799899f9f4ceae88257b32c6d55de0d7012
Davi Müzel <davimuzelleite68085@gmail.com>
2024-06-12 12:41:06 +0000

    T002: style - Inicio da estruturação dos Caards na tela Home

--- appmymovies/src/app/home/home.page.ts
+++ appmymovies/src/app/home/home.page.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/angular/standalone';
+import { TmdbService } from '../services/tmdb/tmdb.service';
+import { Movie } from '../models/movie';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, HttpClientModule, CommonModule, RouterLink, IonList, IonItem, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent ],
+  providers: [TmdbService]
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit{
+  readonly imageSize: string = 'https://image.tmdb.org/t/p/w500/'
+  readonly movies: Movie[] = [];
+
+  constructor(
+    private readonly service: TmdbService,
+  ) {}
+
+  ngOnInit(): void {
+    this.loadMovies();
+  }
+
+  loadMovies() {
+    this.service.getMovies().subscribe({
+      next: (movies: Movie[]) => {
+        this.movies.push(...movies);
+
+        console.log(movies);
+      },
+      error: (erro: any) => {
+        console.error(erro);
+      }
+    });
+  }
 }
