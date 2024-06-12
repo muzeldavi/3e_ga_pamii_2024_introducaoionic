80717c641807bfd0e91bf530922e5bb09a724b5d
Davi Müzel <davimuzelleite68085@gmail.com>
2024-06-12 12:39:38 +0000

    T002: feat - Implementação do serviço TMDB e do Model Movie

---
+++ appmymovies/src/app/models/movie.ts
@@ -0,0 +1,13 @@
+export interface Movie {
+  backdropPath: string;
+  genres: string[];
+  id: number;
+  overview: string;
+  popularity: number;
+  posterPath: string;
+  releaseDate: Date;
+  title: string;
+  video: boolean;
+  voteAverage: number;
+  voteCount: number;
+}
