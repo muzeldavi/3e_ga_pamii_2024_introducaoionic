64fecf0d5b91604c565062ce292b7a7a7496d467
davi muzel <muzelleitedavi@gmail.com>
2024-09-19 12:36:24 +0000

    T006: refact - Aplicação do SoftCode no título na página Home e melhorada a estrutura do código da página MovieDetails

--- appmymovies/src/app/movie-details/movie-details.page.ts
+++ appmymovies/src/app/movie-details/movie-details.page.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonTitle, 
+  IonToolbar, 
+  IonButtons, 
+  IonBackButton 
+} from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -21,6 +28,7 @@ import { ActivatedRoute } from '@angular/router';
   ]
 })
 export class MovieDetailsPage implements OnInit {
+  pageTitle: string = "MyMovies - Detalhes"
   movieId: string = '';
 
   constructor(
