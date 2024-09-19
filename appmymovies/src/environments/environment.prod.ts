5fb950dfab7e2a98514924ec7a692b8420322b02
davi muzel <muzelleitedavi@gmail.com>
2024-09-19 12:38:29 +0000

    T006: config - Ajustados os dcados de ambiente nas variáveis de desenvolvimento e produção

--- appmymovies/src/environments/environment.prod.ts
+++ appmymovies/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
-export const environment = {
-  production: true
+export const env = {
+  production: true,
+
+  TMBD_URL: 'https://api.themoviedb.org/3',
+
+  TMDB_IMG_PATH: 'https://image.tmdb.org/t/p/w300',
+
+  TMBD_TOKEN: 'SEU_TOKEN_TMDB'
 };
